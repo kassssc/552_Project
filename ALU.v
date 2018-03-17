01b601c018df06e9b5fcf0b0c59be29835518e14
williamwang96 <jwang735@wisc.edu>
2018-03-17 14:50:42 -0500

    specified flag order

--- ALU.v
+++ ALU.v
@@ -1,8 +1,8 @@
 module ALU (ALU_in1, ALU_out2, op, ALU_out, flag);
 
-	input	[15:0]	ALU_in1, ALU_out2;
-	input	[2:0]	op;
-	output	[15:0]	ALU_out;
-	output	[2:0]	flag;
+input	[15:0]	ALU_in1, ALU_out2;
+input	[2:0]	op;
+output	[15:0]	ALU_out;
+output	[2:0]	flag;  //MSB is N, then Z, LSB is V
 
 	
