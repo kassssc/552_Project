14978cd93b05399b4e641e1c87cedd95f3e7d2bf
Kass Chupongstimun <k_724@live.com>
2018-03-17 15:11:13 -0500

    Added general.v

--- ALU.v
+++ ALU.v
@@ -1,8 +1,41 @@
-module ALU (ALU_in1, ALU_out2, op, ALU_out, flag);
+module ALU (ALU_in1, ALU_in2, op, ALU_out, flag);
 
 	input	[15:0]	ALU_in1, ALU_out2;
 	input	[2:0]	op;
 	output	[15:0]	ALU_out;
 	output	[2:0]	flag;
 
-	
+	wire	[15:0]	ADDSUB_out, SHIFTER_out, xor, red, paddsb;
+
+	assign xor[15:0] = ALU_in1[15:0] ^ ALU_in2[15:0];
+
+	AddSub_16b addsub_16b (
+
+	);
+
+	Shifter_16b shifter_16b (
+
+	);
+
+	RED_16b red_16b (
+
+	);
+
+	PADDSB_16b paddsb_16b (
+
+	);
+
+	MUX_81_16b sel_ALU_out (
+		.sel(op[2:0]),
+		.i0(ADDSUB_out[15:0]),
+		.i1(ADDSUB_out[15:0]),
+		.i2(red[15:0]),
+		.i3(xor[15:0]),
+		.i4(SHIFTER_out[15:0]),
+		.i5(SHIFTER_out[15:0]),
+		.i6(SHIFTER_out[15:0]),
+		.i7(paddsb[15:0]),
+		.out(ALU_out[15:0])
+	);
+
+endmodule // ALU
---
+++ general.v
@@ -0,0 +1,18 @@
+module MUX_81_16b (sel, i0, i1, i3, i4, i5, i6, i7, out);
+	input	[2:0]	sel;
+	input	[15:0]	i0, i1, i3, i4, i5, i6, i7;
+	output	[15:0]	out;
+	reg		[15:0]	out;
+
+	always @(*)
+	case (sel)
+		3'b000: out = i0;
+		3'b001: out = i1;
+		3'b010: out = i2;
+		3'b011: out = i4;
+		3'b100: out = i4;
+		3'b101: out = i5;
+		3'b110: out = i6;
+		3'b111: out = i7;
+	endcase
+endmodule // mux_81_16b
