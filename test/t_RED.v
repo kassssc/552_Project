050fcaf4814373b04d8dd0590ea7789a64156450
williamwang96 <jwang735@wisc.edu>
2018-03-18 12:56:27 -0500

    make RED work

--- test/t_RED.v
+++ test/t_RED.v
@@ -3,27 +3,20 @@ module t_RED;
 reg[15:0] stimA, stimB; 
 wire[15:0] dest; 
 
-RED_16b UUT(.SrcData1, .SrcData2, .DesData);
+RED_16b UUT(.SrcData1(stimA), .SrcData2(stimB), .DesData(dest));
 
 // stimulus generation
 initial begin
 	stimA = 16'b0;
 	stimB = 16'b0;
 	#50 
-	$display("A:%b\nB:%b\nSum:%b\nFlag:%b\n",stimA,stimB,Sum,flag);
-	// should show zero flag
+	$display("A:%b\nB:%b\nResult:%b\n",stimA,stimB,dest);
 
-	stimA = 16'h8001;
-	stimB = 16'h8002;
+	stimA = 16'hffff;
+	stimB = 16'hffff;
 	#50 
-	$display("A:%b\nB:%b\nSum:%b\nFlag:%b\n",stimA,stimB,Sum,flag);
-	// should show neg flag
-
-	stimA = 16'h8000;
-	stimB = 16'h8000;
-	#50 
-	$display("A:%b\nB:%b\nSum:%b\nFlag:%b\n",stimA,stimB,Sum,flag);
-	// should show ovfl flag
+	$display("A:%b\nB:%b\nResult:%b\n",stimA,stimB,dest);
+	// result should be 
 	
 	#10
 	
