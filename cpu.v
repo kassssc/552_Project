c891798626b1084b102e5d64a51edd0cbdf6c993
kkangle <liu574@wisc.edu>
2018-03-18 14:01:07 -0500

    modified control in cpu

--- cpu.v
+++ cpu.v
@@ -129,7 +129,9 @@ Control_Unit  control_unit(
 	.ALUSrc(ALUSrc),
 	.RegWrite(RegWrite),
 	.hlt(hlt_internal),
-	.pcs(pcs)
+	.pcs(pcs),
+	.ALUOp(ALUOp),
+	.tophalf(tophalf)
 );
 
 // make write register always the first reg in instruction 
