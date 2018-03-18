a0a2d6efa2ed8ceef757ecb4af8d787fa126e639
williamwang96 <jwang735@wisc.edu>
2018-03-18 11:45:42 -0500

    changed mux implementation and update on RED

--- RED_16bit.v
+++ RED_16bit.v
@@ -2,17 +2,34 @@ module RED_16b(input[15:0] SrcData1, input[15:0] SrcData2, output[15:0] DesData)
 
 wire[6:0] pg, gg;
 wire[3:0] reg11, reg22, reg33, reg44, reg12, reg34, reg1234;
-wire[]
+wire[4:0] cout_csa, sum_csa;
 wire[6:0] cout;
 
-
+//
+// Tree of CLAs
+//
+//
+// First level
+//
 CLA_4b add0(.a(SrcData1[3:0]), .b(SrcData2[3:0]), .c_in(1'b0), .pg_out(pg[0]), .gg_out(gg[0]), .s(reg11));
 CLA_4b add1(.a(SrcData1[7:4]), .b(SrcData2[7:4]), .c_in(1'b0), .pg_out(pg[1]), .gg_out(gg[1]), .s(reg22));
 CLA_4b add2(.a(SrcData1[11:8]), .b(SrcData2[11:8]), .c_in(1'b0), .pg_out(pg[2]), .gg_out(gg[2]), .s(reg33));
 CLA_4b add3(.a(SrcData1[15:12]), .b(SrcData2[15:12]), .c_in(1'b0), .pg_out(pg[3]), .gg_out(gg[3]), .s(reg44));
+//
+// Second level
+//
 CLA_4b add4(.a(reg11), .b(reg22), .c_in(1'b0), .pg_out(pg[4]), .gg_out(gg[4]), .s(reg12));
 CLA_4b add5(.a(reg33), .b(reg44), .c_in(1'b0), .pg_out(pg[5]), .gg_out(gg[5]), .s(reg34));
+//
+// Third level
+//
 CLA_4b add4(.a(reg12), .b(reg34), .c_in(1'b0), .pg_out(pg[6]), .gg_out(gg[6]), .s(reg1234));
+//
+// Sum all carries from CLAs
+//
+full_adder_1bit FA0(.A(gg[0]), .B(gg[1]), .Cin(gg[2]), .S(sum_csa[0]), .Cout(cout_csa[0]));
+full_adder_1bit FA1(.A(gg[3]), .B(gg[4]), .Cin(gg[5]), .S(sum_csa[1]), .Cout(cout_csa[0]));
+
 
 
 endmodule // RED_16b
--- general.v
+++ general.v
@@ -1,73 +1,82 @@
-module MUX_81_16b (sel, i0, i1, i2, i3, i4, i5, i6, i7, out);
-	input	[2:0]	sel;
-	input	[15:0]	i0, i1, i2, i3, i4, i5, i6, i7;
-	output	[15:0]	out;
-	reg		[15:0]	out;
-
-	always @(*) begin
-		case (sel)
-			3'b000: out = i0;
-			3'b001: out = i1;
-			3'b010: out = i2;
-			3'b011: out = i3;
-			3'b100: out = i4;
-			3'b101: out = i5;
-			3'b110: out = i6;
-			3'b111: out = i7;
-			default: out = i0;
-		endcase
-	end
-endmodule // mux_81_16b
-
 module MUX_21_1b (sel, i0, i1, out);
-	input	sel, i0, i1;
-	output	out;
-	reg		out;
 
-	always @(*) begin
-		case (sel)
-			1'b0: out = i0;
-			1'b1: out = i1;
-			default: out = i0;
-		endcase
-	end
+input		i0, i1;
+input 		sel;
+output		out;
+reg			inter;
+
+always @(*) begin
+	case (sel)
+		1'b0: inter = i0;
+		1'b1: inter = i1;
+		default: inter = i0;
+	endcase
+end
+
+assign out = inter;
+
+endmodule
+
+module MUX_21_16b (sel, i0, i1, out);
+
+input[15:0]		i0, i1;
+input 			sel;
+output[15:0]	out;
+reg[15:0]		inter;
+
+always@(*) begin
+	case (sel)
+		1'b0: inter = i0;
+		1'b1: inter = i1;
+		default : inter = i0;
+	endcase	
+end
+
+assign out = inter;
+	
 endmodule
 
 module MUX_41_1b (sel, i0, i1, i2, i3, out);
-	input[1:0]	sel;
-	input		i0, i1, i2, i3;
-	output		out;
-	reg 		inter;
 
-	always@(*) begin
-		case (sel)
-			2'b00: inter = i0;
-			2'b01: inter = i1;
-			2'b10: inter = i2;
-			2'b11: inter = i3;
-			default: inter = i0;
-		endcase
-	end
+input[1:0]	sel;
+input		i0, i1, i2, i3;
+output		out;
+reg 		inter;
 
-	assign out = inter;
+always@(*) begin
+	case (sel)
+		2'b00: inter = i0;
+		2'b01: inter = i1;
+		2'b10: inter = i2;
+		2'b11: inter = i3;
+		default: inter = i0;
+	endcase
+end
+
+assign out = inter;
 
 endmodule
 
+module MUX_81_16b (sel, i0, i1, i2, i3, i4, i5, i6, i7, out);
 
-module MUX_21_16b (sel, i0, i1, out);
-	input[15:0]		i0, i1;
-	input 			sel;
-	output[15:0]	out;
-	reg[15:0]		inter;
+input	[2:0]	sel;
+input	[15:0]	i0, i1, i2, i3, i4, i5, i6, i7;
+output	[15:0]	out;
+reg		[15:0]	inter;
 
-	always@(*) begin
-		case (sel)
-			1'b0: inter = i0;
-			1'b1: inter = i1;
-			default : inter = i0;
-		endcase	
-	end
+always @(*) begin
+	case (sel)
+		3'b000: inter = i0;
+		3'b001: inter = i1;
+		3'b010: inter = i2;
+		3'b011: inter = i3;
+		3'b100: inter = i4;
+		3'b101: inter = i5;
+		3'b110: inter = i6;
+		3'b111: inter = i7;
+	endcase
+end
 
-	assign out = inter;
-	
-endmodule
+assign out = inter;
+
+endmodule // mux_81_16b
