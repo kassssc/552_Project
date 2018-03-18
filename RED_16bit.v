7298dd0b0e458bf0570c47256577b3d3e1714626
williamwang96 <jwang735@wisc.edu>
2018-03-18 13:06:39 -0500

    finally fix RED

--- RED_16bit.v
+++ RED_16bit.v
@@ -43,11 +43,11 @@ full_adder_1b FA13(.a(cout_csa[1]), .b(sum_1bcsa[1]), .cin(1'b0), .s(sum_1bcsa[3
 //
 // Level 3, one 2bit FA
 //
-full_adder_2b FA20(.A(cout_csa[2]<<1), .B({{1'b0},{sum_1bcsa[2]}}), .cin({{1'b0},{cout_csa[3]}}), .S(sum_2bcsa), .cout(cout_csa[4]));
+full_adder_2b FA20(.A({{1'b0},cout_csa[2]}<<1), .B({{1'b0},{sum_1bcsa[2]}}), .cin(cout_csa[3]), .S(sum_2bcsa), .cout(cout_csa[4]));
 //
 // Level 4, one 4bit FA
 //
-full_adder_4b FA40(.A({{1'b0},{cout_csa[4]<<2}}), .B({{1'b0},{sum_2bcsa<<1}}), .cin({{2'b0},{sum_1bcsa[3]}}), .S(sum_4bcsa), .cout(cout_csa[5]));
+full_adder_4b FA40(.A({{3'b0},{cout_csa[4]}}<<2), .B({{2'b0},{sum_2bcsa}}<<1), .cin(sum_1bcsa[3]), .S(sum_4bcsa), .cout(cout_csa[5]));
 
 assign DesData = {16{sum_4bcsa}};
 
