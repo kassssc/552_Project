e1ed6ae4924300e697c31b301d066aad3a7bdbaa
kkangle <liu574@wisc.edu>
2018-03-18 20:22:08 -0500

    finally pc worksgit status!

--- PC_Register.v
+++ PC_Register.v
@@ -5,12 +5,9 @@ module PC_Register(
 	output [15:0] PC_current
 );
 
-	wire [15:0] current;
-	assign pc_current = current;
-
 dff PC_bit0(
 	.d(PC_new[0]), 
-	.q(current[0]), 
+	.q(PC_current[0]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -18,7 +15,7 @@ dff PC_bit0(
 
 dff PC_bit1(
 	.d(PC_new[1]), 
-	.q(current[1]), 
+	.q(PC_current[1]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -26,7 +23,7 @@ dff PC_bit1(
 
 dff PC_bit2(
 	.d(PC_new[2]), 
-	.q(current[2]), 
+	.q(PC_current[2]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -34,7 +31,7 @@ dff PC_bit2(
 
 dff PC_bit3(
 	.d(PC_new[3]), 
-	.q(current[3]), 
+	.q(PC_current[3]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -42,7 +39,7 @@ dff PC_bit3(
 
 dff PC_bit4(
 	.d(PC_new[4]), 
-	.q(current[4]), 
+	.q(PC_current[4]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -50,7 +47,7 @@ dff PC_bit4(
 
 dff PC_bit5(
 	.d(PC_new[5]), 
-	.q(current[5]), 
+	.q(PC_current[5]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -58,7 +55,7 @@ dff PC_bit5(
 
 dff PC_bit6(
 	.d(PC_new[6]), 
-	.q(current[6]), 
+	.q(PC_current[6]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -67,7 +64,7 @@ dff PC_bit6(
 
 dff PC_bit7(
 	.d(PC_new[7]), 
-	.q(current[7]), 
+	.q(PC_current[7]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -76,7 +73,7 @@ dff PC_bit7(
 
 dff PC_bit8(
 	.d(PC_new[8]), 
-	.q(current[8]), 
+	.q(PC_current[8]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -85,7 +82,7 @@ dff PC_bit8(
 
 dff PC_bit9(
 	.d(PC_new[9]), 
-	.q(current[9]), 
+	.q(PC_current[9]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -94,7 +91,7 @@ dff PC_bit9(
 
 dff PC_bit10(
 	.d(PC_new[10]), 
-	.q(current[10]), 
+	.q(PC_current[10]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -102,7 +99,7 @@ dff PC_bit10(
 
 dff PC_bit11(
 	.d(PC_new[11]), 
-	.q(current[11]), 
+	.q(PC_current[11]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -110,7 +107,7 @@ dff PC_bit11(
 
 dff PC_bit12(
 	.d(PC_new[12]), 
-	.q(current[12]), 
+	.q(PC_current[12]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -118,7 +115,7 @@ dff PC_bit12(
 
 dff PC_bit13(
 	.d(PC_new[13]), 
-	.q(current[13]), 
+	.q(PC_current[13]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
@@ -126,14 +123,14 @@ dff PC_bit13(
 
 dff PC_bit14(
 	.d(PC_new[14]), 
-	.q(current[14]), 
+	.q(PC_current[14]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
 );
 dff PC_bit15(
 	.d(PC_new[15]), 
-	.q(current[15]), 
+	.q(PC_current[15]), 
 	.wen(1'b1), 
 	.clk(clk), 
 	.rst(rst)
