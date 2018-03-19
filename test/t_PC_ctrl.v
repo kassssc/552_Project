95196fd09c325b7d1762c3803b82f9257f98f123
Kass Chupongstimun <k_724@live.com>
2018-03-18 19:42:27 -0500

    xx

--- test/t_PC_ctrl.v
+++ test/t_PC_ctrl.v
@@ -4,33 +4,22 @@
 module t_pc_ctrl_4b;
 
 	reg	[15:0] pc_in_test;
-	reg [8:0] instr; // [ IIIIIIIII FFF CCC ]
-	reg [2:0] ccc;
+	reg [15:0] instr; // [ IIIIIIIII FFF CCC ]
 
 	wire [15:0] pc_out;
 
 	PC_control ctrl (
-		.C(ccc), .I(instr), .F(3'b000),
+		.C(instr[11:9]), .I(instr[8:0]), .F(3'b000),
 		.PC_in(pc_in_test[15:0]), .PC_out(pc_out[15:0]), .hlt(1'b0)
 	);
 
 	// Test Bench
 	initial begin
 		#50
-		// Regular advance, pc_out = 0xDEAF
-		pc_in_test = 16'hDEAD;
-		instr = 9'h012;
-		ccc = 3'b001;
-		#50
-		// Regular Advance, pc_out = 0xBEF1
-		pc_in_test = 16'hBEEF;
-		instr = 9'h012;
-		ccc = 3'b001;
-		#50
+		// Regular advance, pc_out = 
 		// Jump by 0x10 = 0b00010000 pc_out = 0xCB10
 		pc_in_test = 16'h0000;
-		instr = 9'h012;
-		ccc =3'b001;
+		instr = 16'h0412;
 
 	end
 	
@@ -38,8 +27,8 @@ module t_pc_ctrl_4b;
 	initial begin
 		//$monitor("a = %b\nb = %b\nc_in = %b\ng_out = %b\np_out = %b\ns = %b",
 		//		  test[2], test[1], test[0], g_out, p_out, s);
-		$monitor("pc_in = %h\ninstr = %b\nccc = %b\npc_out = %h\n",
-				  pc_in_test, instr, ccc, pc_out);
+		$monitor("pc_in = %h\ninstr = %h\npc_out = %h\n",
+				  pc_in_test, instr, pc_out);
 	end
 
 endmodule
