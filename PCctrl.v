977fdca0b32ed04d8f47de5ed64deb7093245eca
Kass Chupongstimun <k_724@live.com>
2018-03-18 19:25:19 -0500

    reg branch

--- PCctrl.v
+++ PCctrl.v
@@ -1,15 +1,24 @@
-module PC_control(C, I, F, PC_in, PC_out, hlt);
+module PC_control(C, I, F, PC_in, PC_out, hlt, B, branch_reg_in);
 
 	input hlt;
+	input	[3:0]	B;	// branch instr
 	input	[2:0]	C;	// Condition Encoding
 	input	[2:0]	F;	// [ N V Z ]
 	input	[8:0]	I;
 	input	[15:0]	PC_in;
+	input 	[15:0] 	branch_reg_in;
 	output	[15:0]	PC_out;
+
 
 	wire neg_flag, ovfl_flag, zero_flag, branch;
 	wire NEQ, EQ, GT, LT, GEQ, LEQ, OVFL, UNCOND;
 	wire [15:0] target, sign_extend_I, shifted_I, PC_plus_2;
+
+	wire is_branch_imm;
+	wire is_branch_reg;
+
+	assign is_branch_imm = B[3] & B[2] & ~B[1] & ~B[0];
+	assign is_branch_reg = B[3] & B[2] & ~B[1] & B[0];
 
 	assign neg_flag = F[2];
 	assign ovfl_flag = F[1];
@@ -24,7 +33,8 @@ module PC_control(C, I, F, PC_in, PC_out, hlt);
 	assign OVFL   =  C[2] &  C[1] & ~C[0];
 	assign UNCOND =  C[2] &  C[1] &  C[0];
 
-	assign branch = ( (NEQ & ~zero_flag) |
+	assign branch = ( (B[0] & B[1]) 
+					  (NEQ & ~zero_flag) |
 					  (EQ & zero_flag) |
 					  (GT & ~zero_flag & ~neg_flag) |
 					  (LT & neg_flag) |
@@ -47,6 +57,6 @@ module PC_control(C, I, F, PC_in, PC_out, hlt);
 		.A(PC_plus_2), .B(shifted_I), .sub(1'b0), .S(target), .ovfl(), .neg()
 	);
 
-	assign PC_out = (hlt)? PC_in : ((branch)? target : PC_plus_2);
+	assign PC_out = (hlt)? PC_in : ((is_branch_imm)? ( (branch)? target : PC_plus_2) : ((is_branch_reg)? ( (branch)? branch_reg_in : PC_plus_2)));
 
 endmodule
--- test/t_PC_ctrl.v
+++ test/t_PC_ctrl.v
@@ -4,12 +4,13 @@
 module t_pc_ctrl_4b;
 
 	reg	[15:0] pc_in_test;
-	reg [14:0] others_test; // [ IIIIIIIII FFF CCC ]
+	reg [8:0] instr; // [ IIIIIIIII FFF CCC ]
+	reg [2:0] ccc;
 
 	wire [15:0] pc_out;
 
 	PC_control ctrl (
-		.C(others_test[2:0]), .I(others_test[14:6]), .F(others_test[5:3]),
+		.C(ccc), .I(instr), .F(3'b000),
 		.PC_in(pc_in_test[15:0]), .PC_out(pc_out[15:0]), .hlt(1'b0)
 	);
 
@@ -18,15 +19,18 @@ module t_pc_ctrl_4b;
 		#50
 		// Regular advance, pc_out = 0xDEAF
 		pc_in_test = 16'hDEAD;
-		others_test = 15'b000000000000000;
+		instr = 9'h012;
+		ccc = 3'b001;
 		#50
 		// Regular Advance, pc_out = 0xBEF1
 		pc_in_test = 16'hBEEF;
-		others_test = 15'b000000000000000;
+		instr = 9'h012;
+		ccc = 3'b001;
 		#50
 		// Jump by 0x10 = 0b00010000 pc_out = 0xCB10
-		pc_in_test = 16'hC000;
-		others_test = 15'b000001000000111;
+		pc_in_test = 16'h0000;
+		instr = 9'h012;
+		ccc =3'b001;
 
 	end
 	
@@ -34,8 +38,8 @@ module t_pc_ctrl_4b;
 	initial begin
 		//$monitor("a = %b\nb = %b\nc_in = %b\ng_out = %b\np_out = %b\ns = %b",
 		//		  test[2], test[1], test[0], g_out, p_out, s);
-		$monitor("pc_in = %h\nbranch = %b\npc_out = %h\n",
-				  pc_in_test, others_test[14:6], pc_out);
+		$monitor("pc_in = %h\ninstr = %b\nccc = %b\npc_out = %h\n",
+				  pc_in_test, instr, ccc, pc_out);
 	end
 
 endmodule
