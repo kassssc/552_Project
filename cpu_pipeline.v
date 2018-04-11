4586e2c68d17acbb0d579f615c78ac5e4db0a652
Kass Chupongstimun <k_724@live.com>
2018-04-11 16:42:08 -0500

    small fix

--- cpu_pipeline.v
+++ cpu_pipeline.v
@@ -189,7 +189,6 @@ assign mem_addr_offset = {{12{EX_instr[3]}}, EX_instr[3:0] << 1};
 
 // Mem stage will choose between this and mem read output
 assign EX_reg_write_data = (ALUop)? ALU_out[15:0] :		// ALUop
-						   (LW)?  EX_ALU_in_1[15:0] : // LW, want
 						   (EX_lhb)? lhb_out[15:0] :	// LHB
 						   (EX_llb)? llb_out[15:0] :	// LLB
 						   EX_pc[15:0];					// PCS
