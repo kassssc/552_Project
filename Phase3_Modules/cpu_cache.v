5b70b3dfbea9465b6c43a02edd666c1a274190f6
williamwang96 <jiayu.wang1996@gmail.com>
2018-04-26 16:37:49 -0500

    fix errors

--- Phase3_Modules/cpu_cache.v
+++ Phase3_Modules/cpu_cache.v
@@ -59,7 +59,7 @@ wire stall_hazard;
 // MEM and CACHE
 //------------------------------------------------------------------------------
 wire MemRead, MemWrite, mem_DataValid;
-wire[15:0] mem_data_out, mem_data_in, mem_read_addr;
+wire[15:0] mem_data_out, mem_data_in, mem_read_addr, mem_write_data;
 
 // cache busy means it "wants" to read from mem
 
@@ -70,6 +70,10 @@ wire [15:0] I_cache_mem_read_addr;
 // D-CACHE
 wire D_mem_fetch, D_MemWrite, D_CacheBusy, D_CacheFinish;
 wire [15:0] D_cache_mem_read_addr;
+
+wire [15:0] pc_current, pc_plus_2;
+
+wire [15:0] mem_read_out;
 
 assign mem_read_addr = I_mem_fetch? I_cache_mem_read_addr[15:0] :
 					   D_mem_fetch? D_cache_mem_read_addr[15:0] : 16'h0000;
@@ -156,7 +160,6 @@ CACHE D_CACHE(
 //------------------------------------------------------------------------------
 // IF: INSTRUCTION FETCH STAGE
 //------------------------------------------------------------------------------
-wire [15:0] pc_current, pc_plus_2;
 
 // branch signal from ID stage
 assign IF_pc_new = EX_Branch_current? EX_pc_branch_target : pc_plus_2;
@@ -376,7 +379,6 @@ EX_MEM EXMEM (
 // MEM: MEMORY STAGE
 //------------------------------------------------------------------------------
 wire MemEnable;
-wire [15:0] mem_read_out, mem_write_data;
 
 assign MEM_reg_write_data = MEM_MemToReg? mem_read_out[15:0] : MEM_EX_reg_write_data[15:0];
 assign mem_write_data[15:0] = MEM_ALU_in_1[15:0];
@@ -424,6 +426,6 @@ forward forwarder (
 assign rst = ~rst_n;
 assign flush = EX_Branch_current? 1'b1 : 1'b0;
 assign pc_out = pc_current;
-assign stall = I_stall | D_stall | stall_hazard;
+assign stall = I_CacheBusy | D_CacheBusy | stall_hazard;
 
 endmodule
